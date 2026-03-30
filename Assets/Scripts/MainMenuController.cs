627ed37da05bace5efcc736bd363dae965663bf7
kkulyknait <kkulyk@nait.ca>
2026-03-30 09:18:16 -0600

    Monday March 30 Lesson part 1
    Added UIDocument for main menu
    Added basic buttons
    Added basic scene loading

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    private UIDocument _document;
+    private Button _startButton;
+    private Label _scoreLabel;
+    private Button _quitButton;
+
+    private void Awake()
+    {
+        _document = GetComponent<UIDocument>();
+        _startButton = _document.rootVisualElement.Q<Button>("start-button");
+        _scoreLabel = _document.rootVisualElement.Q<Label>("score-label");
+        _quitButton = _document.rootVisualElement.Q <Button>("quit-button");
+
+        if (_startButton != null)
+        {
+            _startButton.clicked += OnStartClicked;
+        }
+        if (_quitButton != null)
+        {
+            _quitButton.clicked += OnQuitClicked;
+        }
+
+    }
+
+    private void OnStartClicked()
+    {
+        Debug.Log("Start Button Clicked!  Loading into Game");
+        SceneManager.LoadScene(1);
+    }
+
+    private void OnQuitClicked()
+    {
+        Debug.Log("Quit it!");
+        Application.Quit();
+    }
+}
