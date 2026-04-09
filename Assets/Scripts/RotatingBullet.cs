4792fb337d6e5a09c432e5ac78f0799582c63121
kkulyknait <kkulyk@nait.ca>
2026-04-09 08:39:18 -0600

    Thurs April 9 Lesson part 1

---
+++ Assets/Scripts/RotatingBullet.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class RotatingBullet : MonoBehaviour
+{
+    [SerializeField] private float _speed = 10f;  // Speed of the bullet
+    [SerializeField] private float _spinSpeed = 360f;  // Degrees per second
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Destroy(gameObject, 3f); //  Destroy bullet after 3 seconds 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * _speed * Time.deltaTime, Space.World);
+        transform.Rotate(0, 0, _spinSpeed * Time.deltaTime);  // Rotate projectile
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            //  Destroy both enemy and bullet if hit
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
